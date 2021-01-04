be6baea46a0ba6e2c10e3756e3214b5ea3726a5a
Ahmet Cemal Özgezer <ahmet.ozgezer@andasis.com>
2021-01-04 16:35:05 +0300

    load theme using doom way

--- dotfiles/ahmet/emacs/doom/config.el
+++ dotfiles/ahmet/emacs/doom/config.el
@@ -25,7 +25,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-;; (setq doom-theme 'doom-one)
+(setq doom-theme 'modus-operandi)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -327,7 +327,7 @@
 
   :config
   ;; Load the theme of your choice
-  (modus-themes-load-operandi)
+  ;; (modus-themes-load-operandi)
   ;; ;; OR
   ;; (load-theme 'modus-operandi t)
   :bind ("<f5>" . modus-themes-toggle))
