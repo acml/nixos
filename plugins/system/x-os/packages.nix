368d95b03881f42456cf9314a1d662707c7fdf95
Ahmet Cemal Özgezer <ahmet.ozgezer@andasis.com>
2021-01-05 16:48:58 +0300

    update fonts

--- plugins/system/x-os/packages.nix
+++ plugins/system/x-os/packages.nix
@@ -25,42 +25,37 @@ in mkIf cfg.enable {
     efibootmgr
     ncdu
     gnome3.adwaita-icon-theme
-    hicolor-icon-theme #fixes missing redshift tray icon
+    hicolor-icon-theme # fixes missing redshift tray icon
   ];
 
   # Fonts
-  nixpkgs.overlays = [ (import (builtins.fetchTarball https://github.com/lightdiscord/nix-nerd-fonts-overlay/archive/master.tar.gz)) ];
   fonts = {
     enableFontDir = true;
     enableGhostscriptFonts = true;
     fonts = with pkgs; [
-      # dejavu_fonts
-      # fira
-      # fira-code
-      # fira-code-symbols
-      # nerdfonts
-      # noto-fonts
-      # noto-fonts-emoji
-      # ubuntu_font_family
       font-awesome-ttf
-      nerd-fonts.dejavusansmono
-      nerd-fonts.droidsansmono
-      nerd-fonts.firacode
-      nerd-fonts.firamono
-      nerd-fonts.hermit
-      nerd-fonts.inconsolata
-      nerd-fonts.iosevka
-      nerd-fonts.noto
-      nerd-fonts.ubuntu
-      roboto-mono
+      (nerdfonts.override {
+        fonts = [
+          "DejaVuSansMono"
+          "DroidSansMono"
+          "FiraCode"
+          "FiraMono"
+          "Hermit"
+          "Inconsolata"
+          "Iosevka"
+          "Noto"
+          "RobotoMono"
+          "Ubuntu"
+        ];
+      })
       siji
       symbola
     ];
     fontconfig = {
       enable = true;
       defaultFonts = {
-        sansSerif = ["Ubuntu"];
-        monospace = ["Fira Code"];
+        sansSerif = [ "Ubuntu" ];
+        monospace = [ "Fira Code" ];
       };
     };
   };
