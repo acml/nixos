edc18e955610adeea7547443fc2e33faeba96447
Ahmet Cemal Özgezer <ahmet.ozgezer@andasis.com>
2021-01-18 11:20:21 +0300

    new emacs modus theme settings

--- dotfiles/ahmet/emacs/spacemacs/init.el
+++ dotfiles/ahmet/emacs/spacemacs/init.el
@@ -1162,25 +1162,46 @@ confirmation"
     (setq modus-themes-slanted-constructs t
           modus-themes-bold-constructs t
           modus-themes-fringes nil ; {nil,'subtle,'intense}
+
+          ;; Options for `modus-themes-lang-checkers': nil,
+          ;; 'straight-underline, 'subtle-foreground,
+          ;; 'subtle-foreground-straight-underline, 'intense-foreground,
+          ;; 'intense-foreground-straight-underline, 'colored-background
+          modus-themes-lang-checkers nil
+
           modus-themes-mode-line '3d ; {nil,'3d,'moody}
-          modus-themes-syntax 'faint ; Lots of options---continue reading the manual
+
+          ;; Options for `modus-themes-syntax': nil, 'faint,
+          ;; 'yellow-comments, 'green-strings,
+          ;; 'yellow-comments-green-strings, 'alt-syntax,
+          ;; 'alt-syntax-yellow-comments
+          modus-themes-syntax 'faint
+
           modus-themes-intense-hl-line nil
-          modus-themes-paren-match nil ; {nil,'subtle-bold,'intense,'intense-bold}
-          modus-themes-links 'neutral-underline ; Lots of options---continue reading the manual
+          modus-themes-paren-match 'subtle-bold ; {nil,'subtle-bold,'intense,'intense-bold}
+
+          ;; Options for `modus-themes-links': nil, 'faint,
+          ;; 'neutral-underline, 'faint-neutral-underline, 'no-underline,
+          ;; 'underline-only
+          modus-themes-links 'neutral-underline
+
           modus-themes-no-mixed-fonts nil
           modus-themes-prompts 'subtle ; {nil,'subtle,'intense}
           modus-themes-completions 'opinionated ; {nil,'moderate,'opinionated}
           modus-themes-region 'bg-only-no-extend ; {nil,'no-extend,'bg-only,'bg-only-no-extend}
           modus-themes-diffs 'desaturated ; {nil,'desaturated,'fg-only,'bg-only}
           modus-themes-org-blocks nil ; {nil,'grayscale,'rainbow}
-          modus-themes-org-habit 'traffic-light ; {nil,'simplified,'traffic-light}
+          modus-themes-org-habit nil ; {nil,'simplified,'traffic-light}
+
           modus-themes-headings ; Lots of options---continue reading the manual
           '((1 . section)
             (2 . section-no-bold)
             (3 . rainbow-line)
             (t . rainbow-line-no-bold))
-          modus-themes-variable-pitch-headings nil
-          modus-themes-scale-headings nil
+
+          modus-themes-variable-pitch-ui t
+          modus-themes-variable-pitch-headings t
+          modus-themes-scale-headings t
           modus-themes-scale-1 1.1
           modus-themes-scale-2 1.15
           modus-themes-scale-3 1.21
