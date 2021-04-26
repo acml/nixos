fd884ee96917b5d21f3928c0b96ea45be5ff6db8
Ahmet Cemal Özgezer <ahmet.ozgezer@andasis.com>
2021-04-26 17:44:30 +0300

    neovim refactor

--- plugins/users/ahmetde/neovim.nix
+++ plugins/users/ahmetde/neovim.nix
@@ -211,7 +211,7 @@ in {
         let g:nvim_tree_auto_open = 1 "0 by default, opens the tree when typing `vim $DIR` or `vim`
         let g:nvim_tree_auto_close = 1 "0 by default, closes the tree when it's the last window
         let g:nvim_tree_auto_ignore_ft = [ 'startify', 'dashboard' ] "empty by default, don't auto open tree on specific filetypes.
-        let g:nvim_tree_quit_on_open = 1 "0 by default, closes the tree when you open a file
+        let g:nvim_tree_quit_on_open = 0 "0 by default, closes the tree when you open a file
         let g:nvim_tree_follow = 1 "0 by default, this option allows the cursor to be updated when entering a buffer
         let g:nvim_tree_indent_markers = 1 "0 by default, this option shows indent markers when folders are open
         let g:nvim_tree_hide_dotfiles = 1 "0 by default, this option hides files and folders starting with a dot `.`
@@ -272,7 +272,7 @@ in {
         set termguicolors " this variable must be enabled for colors to be applied properly
 
         " a list of groups can be found at `:help nvim_tree_highlight`
-        highlight NvimTreeFolderIcon guibg=blue
+        " highlight NvimTreeFolderIcon guibg=blue
       '';
     };
 
