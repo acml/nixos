266c8ad5dd51433dfb628fe49a7319373861f763
Ahmet Cemal Özgezer <ahmet.ozgezer@andasis.com>
2021-05-21 11:02:36 +0300

    emacs emoji font configuration

--- dotfiles/ahmet/emacs/doom/config.el
+++ dotfiles/ahmet/emacs/doom/config.el
@@ -96,6 +96,13 @@
 ;; (add-to-list 'default-frame-alist '(alpha . (95)))
 ;; (set-frame-parameter (selected-frame) 'alpha '(95))
 
+(defun --set-emoji-font (frame)
+  "Adjust the font settings of FRAME so Emacs can display emoji properly."
+  (set-fontset-font t 'symbol (font-spec :family "Noto Color Emoji") frame)
+  (set-fontset-font t 'symbol (font-spec :family "Segoe UI Emoji") frame 'append)
+  (set-fontset-font t 'symbol (font-spec :family "Symbola") frame 'append))
+(add-hook 'after-make-frame-functions '--set-emoji-font)
+
 (when (daemonp)
   (add-hook 'after-make-frame-functions
             (lambda (frame)
